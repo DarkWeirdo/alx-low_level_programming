aa344760f4d52f724b57ccfb1fb5d504e4249c14
DarkWeirdo <sonic_unleashed@live.com>
2023-08-14 05:52:12 +0300

    taske 4 mod 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void){
-    puts("Programming is like building a multilingual puzzle\n");
-    return 0;
+int main(void)
+/*
+*Main function for the whole thing
+*/
+{
+char Str1[] = '"Programming is like building a multilingual puzzle';
+puts(Str1);
+return (0);
 }
