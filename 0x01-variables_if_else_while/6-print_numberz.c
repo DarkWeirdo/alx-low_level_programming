9f39cf6b21ba1e136d32316d2027ee2f6b1eeaba
DarkWeirdo <sonic_unleashed@live.com>
2023-08-15 01:57:41 +0300

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - main function of task 6 program
+*Return: 0 as requested
+*/
+int main(void)
+{
+int num = 0;
+while (num >= 0 && num <= 9)
+{
+putchar('0' + num);
+num++;
+}
+putchar('\n');
+return (0);
+}
