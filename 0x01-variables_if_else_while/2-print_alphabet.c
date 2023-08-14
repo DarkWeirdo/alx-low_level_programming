b6036e27ab31f64829456f9f7b20aacaf3d7ce1d
DarkWeirdo <sonic_unleashed@live.com>
2023-08-14 23:39:39 +0300

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - main function of task 4 program
+*Return: 0 as requested
+*/
+int main(void)
+{
+char letter = 'a';
+while (letter >= 'a' && letter <= 'z')
+{
+printf("%c", letter);
+}
+return (0);
+}
