1142490565f550cf3ea0998ca76c164c197f06a0
DarkWeirdo <sonic_unleashed@live.com>
2023-08-14 06:07:10 +0300

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - main function of task 4 program
+*Return: 0 as requested
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
