ba5d82c7a4d111b5d6bd9e02f76ccaa46a263a64
DarkWeirdo <sonic_unleashed@live.com>
2023-08-16 20:17:29 +0300

    task 9 mod 9?

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,20 +13,26 @@ while (table_no <= 9)
 while (multi_no <= 9)
 {
 result = table_no * multi_no;
+if (multi_no > 0)
+{
+if (result >= 10)
+_putchar(' ');
+else
+_putchar('  ');
+}
 if (result >= 10)
 {
 _putchar('0' + (result / 10));
 _putchar('0' + (result % 10));
 _putchar(',');
-_putchar(' ');
 }
 else
 {
 _putchar('0' + result);
 _putchar(',');
-_putchar(' ');
-_putchar(' ');
 }
+if (multi_no < 9)
+_putchar(',');
 multi_no++;
 if (multi_no > 9)
 {
