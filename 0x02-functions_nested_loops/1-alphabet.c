1af631cc93727f8e7b9bdc92f484d362e0e53f8b
DarkWeirdo <sonic_unleashed@live.com>
2023-08-16 13:38:38 +0300

    task 1 mod 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 /**
 *main - main function of task
+*print_alphabet - a while loop the uses _putchar to print the alphabet
 *Return: 0 as requested
 */
 void print_alphabet(void)
@@ -12,4 +13,5 @@ while (letter >= 'a' && letter <= 'z')
 _putchar(letter);
 ++letter;
 }
+_putchar('\n');
 }
