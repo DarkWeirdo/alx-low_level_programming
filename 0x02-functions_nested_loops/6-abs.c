f16b14157c98d4a308529412a7e8eaec5722073a
DarkWeirdo <sonic_unleashed@live.com>
2023-08-16 15:03:12 +0300

    task 6 mod 3

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,23 +1,22 @@
 #include "main.h"
 #include <unistd.h>
 /**
-*print_sign - checks if the input is a positive number or negative or zero
+*_abs - checks if the input is a positive number or negative or zero
 * @n: input to be checked
-*_putchar - prints '+', '=', or '0'
 * Return: 1 if positive, 0 when zero, -1 when negative
 */
 int _abs(int n)
 {
 if (n > 0)
 {
-return(n);
+return( n );
 }
 else if (n < 0)
 {
-return(n * (-1));
+return( n * (-1) );
 }
 else
 {
-return(0);
+return( 0 );
 }
 }
