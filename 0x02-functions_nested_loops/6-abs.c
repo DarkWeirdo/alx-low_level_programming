14af473c415eb95c0ba66908728242eb5836e19a
DarkWeirdo <sonic_unleashed@live.com>
2023-08-16 15:01:07 +0300

    task 6 mod 1

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,19 +6,18 @@
 *_putchar - prints '+', '=', or '0'
 * Return: 1 if positive, 0 when zero, -1 when negative
 */
-int _abs(int n)
+int _abs(n)
 {
 if (n > 0)
 {
-_putchar(n);
+return(n);
 }
 else if (n < 0)
 {
-_putchar(n * -1);
+return(n * (-1));
 }
 else
 {
-_putchar('0');
-}
 return(0);
+}
 }
