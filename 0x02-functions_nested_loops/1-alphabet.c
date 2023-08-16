f230dad2035182524267dc9608ebaba43f2656f2
DarkWeirdo <sonic_unleashed@live.com>
2023-08-16 13:29:24 +0300

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*main - main function of task
+*Return: 0 as requested
+*/
+int main(void)
+{
+print_alphabet();
+return (0);
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -11,3 +11,12 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter >= 'a' && letter <= 'z')
+{
+putchar(letter);
+++letter;
+}
+}
