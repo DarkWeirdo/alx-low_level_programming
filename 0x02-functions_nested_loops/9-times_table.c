da53affeaec886bfd44c32f81188544e7fafa987
DarkWeirdo <sonic_unleashed@live.com>
2023-08-16 20:38:21 +0300

    task 9 final

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,49 +1,24 @@
 #include "main.h"
 #include <unistd.h>
 /**
-*times_table - prints a multiplication table
+*times_table - prints a multi_noiplication table
 */
 void times_table(void)
 {
-int table_no = 0;
-int multi_no = 0;
-int result = 0;
-while (table_no <= 9)
+int table_no, multi_no, result;
+for (table_no = 0; table_no <= 9; table_no++)
 {
-while (multi_no <= 9)
+_putchar(48);
+for (multi_no = 1; multi_no < 9; multi_no++)
 {
-result = table_no * multi_no;
-if (multi_no > 0)
-{
-if (result >= 10)
-_putchar(' ');
-else
-{
-_putchar(' ');
-_putchar(' ');
-}
-}
-if (result >= 10)
-{
-_putchar('0' + (result / 10));
-_putchar('0' + (result % 10));
-}
-else
-{
-_putchar('0' + result);
-}
-if (multi_no < 9)
 _putchar(',');
-multi_no++;
-if (multi_no > 9)
-{
-multi_no = 0;
+_putchar(' ');
+if (result <= 9)
+_putchar(' ');
+else
+_putchar((result / 10) + 48);
+_putchar((result % 10) + 48);
+}
 _putchar('\n');
-break;
-}
-}
-table_no++;
-if (table_no > 9)
-break;
 }
 }
