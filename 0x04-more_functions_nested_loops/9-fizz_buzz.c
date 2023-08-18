211dcff457dc9a976880288381791846847f8de4
DarkWeirdo <sonic_unleashed@live.com>
2023-08-18 13:54:09 +0300

    task 9 mod 4

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,23 +13,22 @@ for (count = 1; count <= 100; count++)
 {
 if (count % 3 == 0 && count % 5 != 0)
 {
-printf("Fizz");
+printf(" Fizz");
 }
 else if (count % 5 == 0 && count % 3 != 0)
 {
-printf("Buzz");
+printf(" Buzz");
 }
 else if (count % 3 == 0 && count % 5 == 0)
 {
-printf("FizzBuzz");
+printf(" FizzBuzz");
 }
 else if (count == 1)
 {
 printf("%d", count);
 }
 else
-printf("%d", count);
-putchar(' ');
+printf(" %d", count);
 }
 printf("\n");
 
