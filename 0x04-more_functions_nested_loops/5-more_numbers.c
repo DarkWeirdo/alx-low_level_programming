59045a6c561f2f8e9775928f4d5ca2b94bb6c890
DarkWeirdo <sonic_unleashed@live.com>
2023-08-18 12:58:01 +0300

    task 5 mod 1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,20 +4,15 @@
 */
 void more_numbers(void)
 {
-int num, row, count;
-for (row = 0; row <= 10; row++)
+int row, count;
+for (row = 0; row < 10; row++)
 {
 for (count = 0; count <= 14; count++)
 {
-num = count;
 if (count > 9)
-{
-_putchar(1 + 48);
-num = count % 10;
-}
-_putchar(num + 48);
+_putchar((count / 10) + '0');
+_putchar((count % 10) + '0');
 }
 _putchar('\n');
 }
-
 }
