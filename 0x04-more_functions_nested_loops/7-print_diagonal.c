15a20ba1a25a8ab3143abe51cc54d900e67e9014
DarkWeirdo <sonic_unleashed@live.com>
2023-08-18 12:03:21 +0300

    tasks 4 to 8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_diagonal - prints a diagonal
+* @n: number of lines the will be printed before the diagonal
+*/
+void print_diagonal(int n)
+{
+int postn, space;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (postn = 1; postn <= n; postn++)
+{
+for (space = 1; space <= postn; space++)
+_putchar(' ');
+_putchar(92);
+_putchar('\n');
+}
+}
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_square - prints a square
+* @size: size of square required to be printed
+*/
+void print_square(int size)
+{
+int row, column;
+if (size == 0)
+_putchar('\n');
+for (row = 1; row <= size; row++)
+{
+for (column = 1; column <= size; column++)
+_putchar('#');
+_putchar('\n');
+}
+}
