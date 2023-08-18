8f5cee6c0d3ffd5a4b1eaff6f5171d8639d04846
DarkWeirdo <sonic_unleashed@live.com>
2023-08-18 12:27:35 +0300

    task 7 mod 1

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,17 +5,15 @@
 */
 void print_diagonal(int n)
 {
-int postn, space;
+int postn;
 if (n <= 0)
 _putchar('\n');
 else
 {
 for (postn = 1; postn <= n; postn++)
 {
-for (space = 1; space <= postn; space++)
-_putchar(' ');
 _putchar(92);
-_putchar('\n');
 }
+_putchar('\n');
 }
 }
