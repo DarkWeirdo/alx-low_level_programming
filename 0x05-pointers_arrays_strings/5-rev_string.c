b50e06fcd657d559bcd421295eea4ab8dd9d84a4
DarkWeirdo <sonic_unleashed@live.com>
2023-08-24 14:56:52 +0300

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,17 @@
 void rev_string(char *s)
 {
 
-char rev = s[0];
-int counter = 0;
+char reverse = s[0];
+int count = 0;
 int i;
 
-while (s[counter] != '\0')
-counter++;
-for (i = 0; i < counter; i++)
+while (s[count] != '\0')
+count++;
+for (i = 0; i < count; i++)
 {
-counter--;
-rev = s[i];
-s[i] = s[counter];
-s[counter] = rev;
+count--;
+reverse = s[i];
+s[i] = s[count];
+s[count] = reverse;
 }
 }
