9bf7e95bcbfae327daf2fe35ede5b62bf63342df
DarkWeirdo <sonic_unleashed@live.com>
2024-02-22 04:49:22 +0200

    task 5 style fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,6 +8,7 @@ The grid is a list of lists of integers, where  0 represents water and  1 repres
 The function assumes that the grid is rectangular and completely surrounded by water.
 """
 
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island in the grid.
@@ -18,17 +19,17 @@ def island_perimeter(grid):
     Returns:
     int: The perimeter of the island.
     """
-    perimeter =  0
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] ==  1:
+            if grid[i][j] == 1:
                 # Check the top, bottom, left, and right neighbors
-                if i ==  0 or grid[i -  1][j] ==  0:  # Top
-                    perimeter +=  1
-                if i == len(grid) -  1 or grid[i +  1][j] ==  0:  # Bottom
-                    perimeter +=  1
-                if j ==  0 or grid[i][j -  1] ==  0:  # Left
-                    perimeter +=  1
-                if j == len(grid[i]) -  1 or grid[i][j +  1] ==  0:  # Right
-                    perimeter +=  1
+                if i == 0 or grid[i - 1][j] == 0:  # Top
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:  # Bottom
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:  # Left
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:  # Right
+                    perimeter += 1
     return perimeter
